cecf6c48b48a066e2b4c2abaf7b440bd0776edfe
ZakiyaA <zakiyaalfughi@gmail.com>
2022-08-18 18:49:03 -0400

    fix bug in useEffect

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,8 @@ import logo from '../images/TrollFace.png';
 const Nav = () => {
   return (
     <div className='nav'>
-     
           <img src={logo} alt='logo' />
           <span className='right-text'>Meme Generator</span>
-         
-
     </div>
   )
 }
